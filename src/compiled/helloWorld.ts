9c7c96bc19631cf35f5b1509aa74b6f5a7daf64d
Pavel Murygin <pavel.murygin@gmail.com>
2021-04-13 20:10:01 +0300

    Add a commented function for users to try

--- src/compiled/helloWorld.ts
+++ src/compiled/helloWorld.ts
@@ -3,6 +3,49 @@ import { RequestFlowBuilder } from '@fluencelabs/fluence/dist/api.unstable';
 
 
 
+export async function id(client: FluenceClient): Promise<void> {
+    let request;
+    const promise = new Promise<void>((resolve, reject) => {
+        request = new RequestFlowBuilder()
+            .withRawScript(
+                `
+(xor
+ (seq
+  (call %init_peer_id% ("getDataSrv" "relay") [] relay)
+  (call %init_peer_id% ("op" "identity") [])
+ )
+ (call %init_peer_id% ("errorHandlingSrv" "error") [%last_error%])
+)
+
+            `,
+            )
+            .configHandler((h) => {
+                h.on('getDataSrv', 'relay', () => {
+                    return client.relayPeerId;
+                });
+                h.on('getRelayService', 'hasReleay', () => {// Not Used
+                    return client.relayPeerId !== undefined;
+                });
+                
+                
+                h.on('errorHandlingSrv', 'error', (args) => {
+                    // assuming error is the single argument
+                    const [err] = args;
+                    reject(err);
+                });
+            })
+            .handleScriptError(reject)
+            .handleTimeout(() => {
+                reject('Request timed out');
+            })
+            .build();
+    });
+    await client.initiateFlow(request);
+    return promise;
+}
+      
+
+
 export async function getPeerExternalAddresses(client: FluenceClient, otherNodePeerId: string): Promise<string[]> {
     let request;
     const promise = new Promise<string[]>((resolve, reject) => {
--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,20 @@
 
 import { createClient } from "@fluencelabs/fluence";
 import { testNet } from "@fluencelabs/fluence-network-environment";
-import { getPeerExternalAddresses } from "./compiled/helloWorld";
+import {
+  getPeerExternalAddresses,
+  // getPeerExternalTimestamp,
+} from "./compiled/helloWorld";
 
 const main = async () => {
   const client = await createClient(testNet[0]);
+
   const addresses = await getPeerExternalAddresses(client, client.relayPeerId!);
   console.log("Relay external addresses: ", addresses);
+
+  // const timestamp = await getPeerExternalTimestamp(client, client.relayPeerId!);
+  // console.log("Relay timestamp: ", timestamp);
+
   client.disconnect();
 };
 
