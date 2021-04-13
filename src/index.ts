184ac97ead5759e9c33aefba4cc41e60e7a90ad2
DieMyst <dmitry.shakhtarin@fluence.ai>
2021-04-13 18:22:51 +0300

    examples, update jar, typescript calls all-in-one

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,54 @@
 #!/usr/bin/env node
 
-import { createClient } from "@fluencelabs/fluence";
+import {createClient, registerServiceFunction, SecurityTetraplet} from "@fluencelabs/fluence";
 import { testNet } from "@fluencelabs/fluence-network-environment";
-import { getPeerExternalAddresses } from "./compiled/helloWorld";
+import {helloWorld} from "./compiled/helloWorld";
+import {testFunc} from "./compiled/func";
+import {getPeerExternalAddresses} from "./compiled/some";
+import {dataAliasFunc} from "./compiled/dataAlias";
+import {callArrowFunc} from "./compiled/callArrow";
 
 const main = async () => {
   const client = await createClient(testNet[0]);
-  const addresses = await getPeerExternalAddresses(client, client.relayPeerId!);
-  console.log("Relay external addresses: ", addresses);
-  client.disconnect();
+
+  registerServiceFunction(client, "DataAlias", "get", (args: any[], _) => {
+    return {
+      peerId: "peer id str",
+      name: "name str"
+    }
+  })
+
+  registerServiceFunction(client, "srv", "str", (args: any[], _) => {
+    return `some str` as unknown as object
+  })
+
+  registerServiceFunction(client, "StringExtra", "addNameToHello", (args: any[], _) => {
+    return `Hello, ${args[0]}!` as unknown as object
+  })
+
+  registerServiceFunction(client, "println", "print", (args: any[], _) => {
+    console.log("println:   " + args[0])
+    return {}
+  })
+
+  const hello = await helloWorld(client, "NAME");
+  console.log("Message: ", hello);
+
+  const res = await testFunc(client);
+  console.log("Message: ", res);
+
+  const addresses = await getPeerExternalAddresses(client, client.relayPeerId!)
+  console.log("Addresses: ", addresses);
+
+  const peerId = await dataAliasFunc(client)
+  console.log("PeerId: ", addresses);
+
+  await callArrowFunc(client, (a: string) => {
+    return "Hello, " + a + "!"
+  })
+
+  await client.disconnect();
+  process.exit(0)
 };
 
 main();
