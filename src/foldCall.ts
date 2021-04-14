081d7914175e6922e6ce561cea1471bf600c174d
DieMyst <dmitry.shakhtarin@fluence.ai>
2021-04-14 17:58:39 +0300

    rename

--- src/foldCall.ts
+++ src/foldCall.ts
@@ -1,7 +1,7 @@
     import {FluenceClient} from "@fluencelabs/fluence";
-    import {foldFunc, parFoldFunc} from "./compiled/fold";
+    import {iterateAndPrint, iterateAndPrintParallel} from "./compiled/fold";
 
     export async function foldCall(client: FluenceClient) {
-        await foldFunc(client, ["1", "2", "3"])
-        await parFoldFunc(client, ["4", "5", "6"])
+        await iterateAndPrint(client, ["1", "2", "3"])
+        await iterateAndPrintParallel(client, ["4", "5", "6"])
     }
