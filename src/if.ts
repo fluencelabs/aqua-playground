f14506a7a0f375d3fec2eb050b0aa5615b977a83
DieMyst <dmitry.shakhtarin@fluence.ai>
2021-04-15 12:33:58 +0300

    add check with numbers

--- src/if.ts
+++ src/if.ts
@@ -1,7 +1,10 @@
 import {FluenceClient} from "@fluencelabs/fluence";
-import {ifElseCall} from "./compiled/if";
+import {ifElseCall, ifElseNumCall} from "./compiled/if";
 
 export async function ifCall(client: FluenceClient) {
     await ifElseCall(client, false)
     await ifElseCall(client, true)
+
+    await ifElseNumCall(client, 1)
+    await ifElseNumCall(client, 5)
 }
