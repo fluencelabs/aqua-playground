d119aa87d6fd398857ade0b8f7fdbbaf9404fe68
boneyard93501 <4523011+boneyard93501@users.noreply.github.com>
2021-04-16 17:26:45 -0500

    fix path

--- src/onCall.ts
+++ src/onCall.ts
@@ -1,5 +1,5 @@
-import {FluenceClient, registerServiceFunction} from "@fluencelabs/fluence";
-import {getPeerExternalAddresses} from "./compiled/some";
+import { FluenceClient, registerServiceFunction } from "@fluencelabs/fluence";
+import { getPeerExternalAddresses } from "./compiled/on";
 
 export async function onCall(client: FluenceClient) {
     const addresses = await getPeerExternalAddresses(client, client.relayPeerId!)
