4cbadf18ac190305bc1a43f63c770ebd455981dc
DieMyst <dmitry.shakhtarin@fluence.ai>
2021-06-01 18:47:54 +0300

    wait for two results

--- src/index.ts
+++ src/index.ts
@@ -7,30 +7,32 @@ import {setKeyPutValue, getValues, putHostValue, clearHostValue} from "./compile
 
 
 const main = async () => {
-    setLogLevel("error")
+    setLogLevel("warn")
     // each compiled aqua function require a connected client
     const client = await createClient(krasnodar[0]);
+
 
     // example to how register a local service
     // it could be used in aqua code as follows
     // service StringExtra("service-id"):
     //     addNameToHello: string -> string
     // see more in helloWorld.aqua
-    registerServiceFunction(client, "service-id", "addNameToHello", (args: any[], _) => {
-        return `Hello, ${args[0]}!`
+    registerServiceFunction(client, "aqua-dht", "merge_two", (args: any[], _) => {
+        console.log("aquadht")
+        console.log(args)
+        return []
     })
 
     console.log("SET KEY")
-    // await putHostValue(client, krasnodar[0].peerId, "Test Hello2", "some value2", [client.relayPeerId!], [], 0);
-    await clearHostValue(client, "Test Hello2", krasnodar[0].peerId);
+    // await setKeyPutValue(client, krasnodar[0].peerId, "Test Hello3", "some value4", client.relayPeerId!, 0);
+    // await clearHostValue(client, "Test Hello2", krasnodar[0].peerId);
     // console.log(nodes.length)
     console.log("GET VALUES")
 
-
-    let values = await getValues(client, krasnodar[0].peerId, "Test Hello2");
+    console.time("ack")
+    let values = await getValues(client, krasnodar[0].peerId, "Test Hello3");
+    console.timeEnd("ack")
     console.dir(values)
-    console.log(values.length)
-
 
     process.exit(0)
 };
