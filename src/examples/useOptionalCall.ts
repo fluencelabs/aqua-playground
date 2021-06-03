821986d6cbff95f88e8cc26c43d44af1b9245549
DieMyst <dmitry.shakhtarin@fluence.ai>
2021-06-03 10:03:08 +0300

    clear println

--- src/examples/useOptionalCall.ts
+++ src/examples/useOptionalCall.ts
@@ -4,14 +4,10 @@ import {useOptional} from "../compiled/examples/option";
 
 export async function useOptionalCall(client: FluenceClient): Promise<string> {
     registerServiceFunction(client, "test2", "getStr", (args: any[], _) => {
-        console.log("getStr:")
-        console.log(args)
         return args[0]
     })
 
     registerServiceFunction(client, "test2", "getStr2", (args: any[], _) => {
-        console.log("getStr2:")
-        console.log(args)
         return args[0]
     })
 
