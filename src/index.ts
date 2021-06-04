ec7583db9294b5a015ab9c40e8108d103425a182
DieMyst <dmitry.shakhtarin@fluence.ai>
2021-06-04 16:13:57 +0300

    update

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,22 @@
 #!/usr/bin/env node
 
-import {createClient, registerServiceFunction, setLogLevel} from "@fluencelabs/fluence";
+import {createClient, FluenceClient, registerServiceFunction, setLogLevel} from "@fluencelabs/fluence";
 
 import {krasnodar} from "@fluencelabs/fluence-network-environment";
 import {getValuesByKey, rkpv, stuffOnProviders} from "./compiled/dht-use";
+import {registerKeyPutHostValue} from "@fluencelabs/aqua-dht";
 
 
 
 const main = async () => {
     setLogLevel("info")
     // each compiled aqua function require a connected client
-    const client = await createClient(krasnodar[0]);
+    const client: FluenceClient = await createClient(krasnodar[0]);
 
     let key = "Test Hello3"
     let nodeId = krasnodar[0].peerId
     console.log("SET KEY")
-    await rkpv(client, nodeId, key, "some value21", krasnodar[0].peerId, null);
+    await registerKeyPutHostValue(client, nodeId, key, "some value21", null);
     let recs = getValuesByKey(client, nodeId, key)
     console.log(recs)
     console.log("GET VALUES")
