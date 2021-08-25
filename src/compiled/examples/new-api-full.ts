9c0a0aaabad19beec804f05d447ab169374bed3f
Pavel <Pavel.Murygin@gmail.com>
2021-08-25 15:08:56 +0300

    fix TS build issue

--- src/compiled/examples/new-api-full.ts
+++ src/compiled/examples/new-api-full.ts
@@ -16,27 +16,29 @@ import {
 
 // Services
 
-export function registerHelloWorld(service: { hello: (s: string, callParams: CallParams<'s'>) => void }): void;
-export function registerHelloWorld(
+export function registerServiceWithDefaultId(service: {
+    hello: (s: string, callParams: CallParams<'s'>) => void;
+}): void;
+export function registerServiceWithDefaultId(
     serviceId: string,
     service: {
         hello: (s: string, callParams: CallParams<'s'>) => void;
     },
 ): void;
-export function registerHelloWorld(
+export function registerServiceWithDefaultId(
     peer: FluencePeer,
     service: {
         hello: (s: string, callParams: CallParams<'s'>) => void;
     },
 ): void;
-export function registerHelloWorld(
+export function registerServiceWithDefaultId(
     peer: FluencePeer,
     serviceId: string,
     service: {
         hello: (s: string, callParams: CallParams<'s'>) => void;
     },
 ): void;
-export function registerHelloWorld(...args) {
+export function registerServiceWithDefaultId(...args) {
     let peer: FluencePeer;
     let serviceId;
     let service;
@@ -51,7 +53,66 @@ export function registerHelloWorld(...args) {
     } else if (typeof args[1] === 'string') {
         serviceId = args[1];
     } else {
-        serviceId = 'hello_world';
+        serviceId = 'defaultId';
+    }
+
+    if (!(args[0] instanceof FluencePeer) && typeof args[0] === 'object') {
+        service = args[0];
+    } else if (typeof args[1] === 'object') {
+        service = args[1];
+    } else {
+        service = args[2];
+    }
+
+    peer.callServiceHandler.use((req, resp, next) => {
+        if (req.serviceId !== serviceId) {
+            next();
+            return;
+        }
+
+        if (req.fnName === 'hello') {
+            const callParams = {
+                ...req.particleContext,
+                tetraplets: {
+                    s: req.tetraplets[0],
+                },
+            };
+            resp.retCode = ResultCodes.success;
+            service.hello(req.args[0], callParams);
+            resp.result = {};
+        }
+
+        next();
+    });
+}
+
+export function registerServiceWithOUTDefaultId(
+    serviceId: string,
+    service: {
+        hello: (s: string, callParams: CallParams<'s'>) => void;
+    },
+): void;
+export function registerServiceWithOUTDefaultId(
+    peer: FluencePeer,
+    serviceId: string,
+    service: {
+        hello: (s: string, callParams: CallParams<'s'>) => void;
+    },
+): void;
+export function registerServiceWithOUTDefaultId(...args) {
+    let peer: FluencePeer;
+    let serviceId;
+    let service;
+    if (args[0] instanceof FluencePeer) {
+        peer = args[0];
+    } else {
+        peer = FluencePeer.default;
+    }
+
+    if (typeof args[0] === 'string') {
+        serviceId = args[0];
+    } else if (typeof args[1] === 'string') {
+        serviceId = args[1];
     }
 
     if (!(args[0] instanceof FluencePeer) && typeof args[0] === 'object') {
@@ -202,141 +263,6 @@ export function registerMoreMembers(...args) {
 
 // Functions
 
-export async function sayHello(config?: { ttl?: number }): Promise<void>;
-export async function sayHello(peer: FluencePeer, config?: { ttl?: number }): Promise<void>;
-export async function sayHello(...args) {
-    let peer: FluencePeer;
-
-    let config;
-    if (args[0] instanceof FluencePeer) {
-        peer = args[0];
-        config = args[1];
-    } else {
-        peer = FluencePeer.default;
-        config = args[0];
-    }
-
-    let request: RequestFlow;
-    const promise = new Promise<void>((resolve, reject) => {
-        const r = new RequestFlowBuilder()
-            .disableInjections()
-            .withRawScript(
-                `
-     (xor
- (seq
-  (call %init_peer_id% ("getDataSrv" "-relay-") [] -relay-)
-  (call %init_peer_id% ("hello_world" "hello") ["hello, world"])
- )
- (call %init_peer_id% ("errorHandlingSrv" "error") [%last_error% 1])
-)
-
-                 `,
-            )
-            .configHandler((h) => {
-                h.on('getDataSrv', '-relay-', () => {
-                    return peer.connectionInfo.connectedRelays[0];
-                });
-
-                h.onEvent('callbackSrv', 'response', (args) => {});
-
-                h.onEvent('errorHandlingSrv', 'error', (args) => {
-                    const [err] = args;
-                    reject(err);
-                });
-            })
-            .handleScriptError(reject)
-            .handleTimeout(() => {
-                reject('Request timed out for sayHello');
-            });
-        if (config && config.ttl) {
-            r.withTTL(config.ttl);
-        }
-        request = r.build();
-    });
-    await peer.initiateFlow(request!);
-    return Promise.race([promise, Promise.resolve()]);
-}
-
-export async function callBackZeroArgs(
-    callback: (callParams: CallParams<null>) => void,
-    config?: { ttl?: number },
-): Promise<void>;
-export async function callBackZeroArgs(
-    peer: FluencePeer,
-    callback: (callParams: CallParams<null>) => void,
-    config?: { ttl?: number },
-): Promise<void>;
-export async function callBackZeroArgs(...args) {
-    let peer: FluencePeer;
-    let callback;
-    let config;
-    if (args[0] instanceof FluencePeer) {
-        peer = args[0];
-        callback = args[1];
-        config = args[2];
-    } else {
-        peer = FluencePeer.default;
-        callback = args[0];
-        config = args[1];
-    }
-
-    let request: RequestFlow;
-    const promise = new Promise<void>((resolve, reject) => {
-        const r = new RequestFlowBuilder()
-            .disableInjections()
-            .withRawScript(
-                `
-     (xor
- (seq
-  (call %init_peer_id% ("getDataSrv" "-relay-") [] -relay-)
-  (xor
-   (call %init_peer_id% ("callbackSrv" "callback") [])
-   (call %init_peer_id% ("errorHandlingSrv" "error") [%last_error% 1])
-  )
- )
- (call %init_peer_id% ("errorHandlingSrv" "error") [%last_error% 2])
-)
-
-                 `,
-            )
-            .configHandler((h) => {
-                h.on('getDataSrv', '-relay-', () => {
-                    return peer.connectionInfo.connectedRelays[0];
-                });
-
-                h.use((req, resp, next) => {
-                    if (req.serviceId === 'callbackSrv' && req.fnaAme === 'callback') {
-                        const callParams = {
-                            ...req.particleContext,
-                            tetraplets: {},
-                        };
-                        resp.retCode = ResultCodes.success;
-                        callback(callParams);
-                        resp.result = {};
-                    }
-                    next();
-                });
-
-                h.onEvent('callbackSrv', 'response', (args) => {});
-
-                h.onEvent('errorHandlingSrv', 'error', (args) => {
-                    const [err] = args;
-                    reject(err);
-                });
-            })
-            .handleScriptError(reject)
-            .handleTimeout(() => {
-                reject('Request timed out for callBackZeroArgs');
-            });
-        if (config && config.ttl) {
-            r.withTTL(config.ttl);
-        }
-        request = r.build();
-    });
-    await peer.initiateFlow(request!);
-    return Promise.race([promise, Promise.resolve()]);
-}
-
 export async function f1(
     callback: (arg0: string, arg1: number, callParams: CallParams<'arg0' | 'arg1'>) => void,
     config?: { ttl?: number },
@@ -410,6 +336,100 @@ export async function f1(...args) {
             .handleScriptError(reject)
             .handleTimeout(() => {
                 reject('Request timed out for f1');
+            });
+        if (config && config.ttl) {
+            r.withTTL(config.ttl);
+        }
+        request = r.build();
+    });
+    await peer.initiateFlow(request!);
+    return Promise.race([promise, Promise.resolve()]);
+}
+
+export async function f2(
+    num: number,
+    callback: (arg0: string, arg1: number, callParams: CallParams<'arg0' | 'arg1'>) => void,
+    config?: { ttl?: number },
+): Promise<void>;
+export async function f2(
+    peer: FluencePeer,
+    num: number,
+    callback: (arg0: string, arg1: number, callParams: CallParams<'arg0' | 'arg1'>) => void,
+    config?: { ttl?: number },
+): Promise<void>;
+export async function f2(...args) {
+    let peer: FluencePeer;
+    let num;
+    let callback;
+    let config;
+    if (args[0] instanceof FluencePeer) {
+        peer = args[0];
+        num = args[1];
+        callback = args[2];
+        config = args[3];
+    } else {
+        peer = FluencePeer.default;
+        num = args[0];
+        callback = args[1];
+        config = args[2];
+    }
+
+    let request: RequestFlow;
+    const promise = new Promise<void>((resolve, reject) => {
+        const r = new RequestFlowBuilder()
+            .disableInjections()
+            .withRawScript(
+                `
+     (xor
+ (seq
+  (seq
+   (call %init_peer_id% ("getDataSrv" "-relay-") [] -relay-)
+   (call %init_peer_id% ("getDataSrv" "num") [] num)
+  )
+  (xor
+   (call %init_peer_id% ("callbackSrv" "callback") ["hello, world" 42])
+   (call %init_peer_id% ("errorHandlingSrv" "error") [%last_error% 1])
+  )
+ )
+ (call %init_peer_id% ("errorHandlingSrv" "error") [%last_error% 2])
+)
+
+                 `,
+            )
+            .configHandler((h) => {
+                h.on('getDataSrv', '-relay-', () => {
+                    return peer.connectionInfo.connectedRelays[0];
+                });
+                h.on('getDataSrv', 'num', () => {
+                    return num;
+                });
+
+                h.use((req, resp, next) => {
+                    if (req.serviceId === 'callbackSrv' && req.fnaAme === 'callback') {
+                        const callParams = {
+                            ...req.particleContext,
+                            tetraplets: {
+                                arg0: req.tetraplets[0],
+                                arg1: req.tetraplets[1],
+                            },
+                        };
+                        resp.retCode = ResultCodes.success;
+                        callback(req.args[0], req.args[1], callParams);
+                        resp.result = {};
+                    }
+                    next();
+                });
+
+                h.onEvent('callbackSrv', 'response', (args) => {});
+
+                h.onEvent('errorHandlingSrv', 'error', (args) => {
+                    const [err] = args;
+                    reject(err);
+                });
+            })
+            .handleScriptError(reject)
+            .handleTimeout(() => {
+                reject('Request timed out for f2');
             });
         if (config && config.ttl) {
             r.withTTL(config.ttl);
@@ -523,32 +543,27 @@ export async function f3(...args) {
     return promise;
 }
 
-export async function f2(
-    num: number,
-    callback: (arg0: string, arg1: number, callParams: CallParams<'arg0' | 'arg1'>) => void,
+export async function callBackZeroArgs(
+    callback: (callParams: CallParams<null>) => void,
     config?: { ttl?: number },
 ): Promise<void>;
-export async function f2(
+export async function callBackZeroArgs(
     peer: FluencePeer,
-    num: number,
-    callback: (arg0: string, arg1: number, callParams: CallParams<'arg0' | 'arg1'>) => void,
+    callback: (callParams: CallParams<null>) => void,
     config?: { ttl?: number },
 ): Promise<void>;
-export async function f2(...args) {
+export async function callBackZeroArgs(...args) {
     let peer: FluencePeer;
-    let num;
     let callback;
     let config;
     if (args[0] instanceof FluencePeer) {
         peer = args[0];
-        num = args[1];
-        callback = args[2];
-        config = args[3];
-    } else {
-        peer = FluencePeer.default;
-        num = args[0];
         callback = args[1];
         config = args[2];
+    } else {
+        peer = FluencePeer.default;
+        callback = args[0];
+        config = args[1];
     }
 
     let request: RequestFlow;
@@ -559,12 +574,9 @@ export async function f2(...args) {
                 `
      (xor
  (seq
-  (seq
-   (call %init_peer_id% ("getDataSrv" "-relay-") [] -relay-)
-   (call %init_peer_id% ("getDataSrv" "num") [] num)
-  )
+  (call %init_peer_id% ("getDataSrv" "-relay-") [] -relay-)
   (xor
-   (call %init_peer_id% ("callbackSrv" "callback") ["hello, world" 42])
+   (call %init_peer_id% ("callbackSrv" "callback") [])
    (call %init_peer_id% ("errorHandlingSrv" "error") [%last_error% 1])
   )
  )
@@ -577,21 +589,15 @@ export async function f2(...args) {
                 h.on('getDataSrv', '-relay-', () => {
                     return peer.connectionInfo.connectedRelays[0];
                 });
-                h.on('getDataSrv', 'num', () => {
-                    return num;
-                });
 
                 h.use((req, resp, next) => {
                     if (req.serviceId === 'callbackSrv' && req.fnaAme === 'callback') {
                         const callParams = {
                             ...req.particleContext,
-                            tetraplets: {
-                                arg0: req.tetraplets[0],
-                                arg1: req.tetraplets[1],
-                            },
+                            tetraplets: {},
                         };
                         resp.retCode = ResultCodes.success;
-                        callback(req.args[0], req.args[1], callParams);
+                        callback(callParams);
                         resp.result = {};
                     }
                     next();
@@ -606,7 +612,7 @@ export async function f2(...args) {
             })
             .handleScriptError(reject)
             .handleTimeout(() => {
-                reject('Request timed out for f2');
+                reject('Request timed out for callBackZeroArgs');
             });
         if (config && config.ttl) {
             r.withTTL(config.ttl);
