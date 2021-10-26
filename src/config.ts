a8ed9d6b7afffa33f3016ebd8e87de041adbff7e
DieMyst <dmitry.shakhtarin@fluence.ai>
2021-10-26 17:47:30 +0300

    krasnodar -> testNet

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 import { krasnodar, testNet } from '@fluencelabs/fluence-network-environment';
 
-export const relays = krasnodar;
-// export const relays = testNet;
+// export const relays = krasnodar;
+export const relays = testNet;
