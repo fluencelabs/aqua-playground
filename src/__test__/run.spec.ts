2e75be42c6fb1744777378491ab4085fe4685bc3
DieMyst <dmitry.shakhtarin@fluence.ai>
2021-11-12 16:05:04 +0300

    add run test to test suite

---
+++ src/__test__/run.spec.ts
@@ -0,0 +1,37 @@
+import {callArrowCall} from "../examples/callArrowCall";
+import {krasnodar, stage, testNet} from '@fluencelabs/fluence-network-environment';
+
+import { exec } from "child_process";
+
+
+describe('Testing run command', () => {
+
+    const addr = krasnodar[2].multiaddr
+    const nodeId = krasnodar[3].peerId
+    const message = "hello"
+    const func = `\"call(\\\"${message}\\\", \\\"${nodeId}\\\")\"`
+    const call = `npm run aqua run -- --addr ${addr} -i cli-run-aqua/caller.aqua -m node_modules/ --func ${func}`
+
+    it('run simple command', async () => {
+        exec(call, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`error: ${error.message}`);
+                process.exit(1)
+            }
+            if (stderr) {
+                console.error(`stderr: ${stderr}`);
+                process.exit(1)
+            }
+
+            // get element before last in output
+            const result = stdout.split("\n").slice(-2)[0]
+            if (result === message) {
+                console.log("Test passed.")
+                process.exit(0)
+            } else {
+                console.log(`Incorrect message returned. Returned: ${result}. Expected: ${message}`)
+                process.exit(1)
+            }
+        });
+    });
+})
