3b8ca0819c06441ce9a4906a35b8ee05c762ed0b
DieMyst <dmitry.shakhtarin@fluence.ai>
2021-11-16 23:47:44 +0300

    update string

--- src/config.ts
+++ src/config.ts
@@ -5,14 +5,14 @@ export const krasnodarConfig = {
     relays: krasnodar,
     externalAddressesRelay1: ['/ip4/164.90.171.139/tcp/7770', '/ip4/164.90.171.139/tcp/9990/ws'],
     externalAddressesRelay2: ["/ip4/164.90.164.229/tcp/7001", "/ip4/164.90.164.229/tcp/9001/ws"],
-    tryCatchError: "Error: Service with id 'unex' not found"
+    tryCatchError: "Local service error, ret_code is 1, error message is '\"Service with id 'unex' not found (function getStr)\"'"
 }
 
 export const stageConfig = {
     relays: stage,
     externalAddressesRelay1: ["/ip4/134.209.186.43/tcp/7001", "/ip4/134.209.186.43/tcp/9001/ws"],
     externalAddressesRelay2: ["/ip4/134.209.186.43/tcp/7770", "/ip4/134.209.186.43/tcp/9990/ws"],
-    tryCatchError: "Error: Service with id 'unex' not found"
+    tryCatchError: "Local service error, ret_code is 1, error message is '\"Service with id 'unex' not found (function getStr)\"'"
 }
 
 export const testNetConfig = {
